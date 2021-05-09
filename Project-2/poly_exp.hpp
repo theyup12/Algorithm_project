f7700b050fe6eb5200618929d6d92d08115c1865
theyup12 <dongyicao123@csu.fullerton.edu>
2021-05-09 14:27:02 -0700

    finish

--- Project-2/poly_exp.hpp
+++ Project-2/poly_exp.hpp
@@ -72,25 +72,71 @@ public:
 summed_span max_subarray_exh(const std::vector<int>& input) {
 
   assert(!input.empty());
-
-  // TODO: Rewrite the body of this function so that it actually works. That
-  // includes rewriting the return statement. After you do that, delete this
-  // comment.
-  //  return summed_span(input.begin(), input.begin() + 1);
-  return summed_span(input.begin(), input.end());
+  int b = 0;
+  int e = 1; 
+  for(int i = 0; i <= input.size() - 1; i++){
+    for(int j = i + 1; j <= input.size(); j++){
+      summed_span sub_vec(input.begin() + i, input.begin() + j);
+      summed_span sub_vec1(input.begin() + b, input.begin() + e);
+      if(sub_vec.sum() > sub_vec1.sum()){    
+        b = i;
+        e = j; 
+      }
+    }
+  }
+  return summed_span(input.begin() + b , input.begin() + e);
 }
+summed_span maximum_subarray_crossing(const std::vector<int>& vec, int clow, int cmiddle, int chigh){
+  int left_sum =-999;
+  int right_sum = -999;
+  int sum = 0;
+  int b =0;
+  int e = 0;
+  for(int i = cmiddle ; i >= clow; i--){
+    sum += vec[i];
+    if (sum > left_sum){
+      left_sum = sum;
+      b = i;
+    }
+  }
+  sum = 0;
+  for(int i = cmiddle + 1; i <= chigh; i++){
+    sum += vec[i];
+    if(sum > right_sum){
+      right_sum = sum;
+      e = i;
+    }
+  }
+  return summed_span(vec.begin() + b, vec.begin() + (e + 1));
+}
+summed_span maximum_subarray_recurse(const std::vector<int>& V, int low, int high){
+  if (low == high){
+    return summed_span(V.begin() + low, V.begin() + low + 1);
+  }
+  int middle = (low + high) / 2;
+  summed_span entirely_left = maximum_subarray_recurse(V, low, middle); 
+  summed_span entirely_right = maximum_subarray_recurse(V, middle + 1, high);
+  summed_span crossing = maximum_subarray_crossing(V, low, middle, high);
+  if(entirely_left.sum() >= entirely_right.sum() && entirely_left.sum() >= crossing.sum()){
+    
+    return entirely_left;
 
+  }else if (entirely_right.sum() >= entirely_left.sum() && entirely_right.sum() >= crossing.sum()){
+    
+    return entirely_right;
+  }else{
+    return crossing;
+  }
+}
 // Compute the maximum subarray using a decrease-by-half algorithm that takes
 // O(n log n) time.
 summed_span max_subarray_dbh(const std::vector<int>& input) {
 
   assert(!input.empty());
 
-  // TODO: Rewrite the body of this function so that it actually works. That
-  // includes rewriting the return statement. After you do that, delete this
-  // comment.
-  //  return summed_span(input.begin(), input.begin() + 1);
-  return summed_span(input.begin(), input.end());
+  summed_span result = maximum_subarray_recurse(input, 0, input.size() - 1);
+  
+  return result;
 }
 
 // Solve the subset sum problem: return a non-empty subset of input that adds
@@ -99,16 +145,27 @@ summed_span max_subarray_dbh(const std::vector<int>& input) {
 // Note that the returned subset must never be empty, even if target == 0.
 // This uses an exhaustive search algorithm that takes exponential O(n * 2^n)
 // time.
-std::optional<std::vector<int>>
-subset_sum_exh(const std::vector<int>& input, int target) {
+std::optional<std::vector<int>>subset_sum_exh(const std::vector<int>& input, int target) {
 
   assert(!input.empty());
   assert(input.size() < 64);
-
-  // TODO: Rewrite the body of this function so that it actually works. That
-  // includes rewriting the return statement. After you do that, delete this
-  // comment.
-  //return std::make_optional<std::vector<int>>();
+  int total = 0;
+  int n = input.size();
+  std::optional<std::vector<int>> candidate;
+  for(int bits = 0; bits <= (pow(2, n) - 1); bits++){
+    std::vector<int> vec = {};
+    for(int j = 0; j <= n - 1; j++){
+      if((bits >> j & 1) == 1){
+        vec.push_back(input[j]); 
+      }
+      total = accumulate(vec.begin(), vec.end(), 0);
+      
+      if(vec.size() > 0 && total == target){
+        candidate = vec;
+        return candidate; 
+      }
+    }
+  }
   return std::nullopt;
  }
 
